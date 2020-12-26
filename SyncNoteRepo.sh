822b92b2b7c209c8b104447940571aa87de62cb3
Air4x <mario.calacagno03@gmail.com>
2020-12-26 18:37:12 +0100

    Added a little script to copy the script from the system to the repo

---
+++ SyncNoteRepo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Coping Note-Taking.sh"
+cp $HOME/scripts/Note-Taking.sh $HOME/Documenti/Progetti/Shell/Note-Taking/ && echo -e "\tNote-Taking.sh copied"
+cp $HOME/scripts/Create-Note.sh $HOME/Documenti/Progetti/Shell/Note-Taking/ && echo -e "\tCreate-Note.sh copied"
+cp $HOME/scripts/SyncNoteRepo.sh $HOME/Documenti/Progetti/Shell/Note-Taking/ && echo -e "\tSyncNoteRepo.sh copied"
