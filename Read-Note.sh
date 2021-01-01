a0b5b3269385da90cac619da4d47faaea5359e85
Air4x <mario.calacagno03@gmail.com>
2021-01-01 19:47:59 +0100

    sorting file name for time in Read-Note.sh

--- Read-Note.sh
+++ Read-Note.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
 Notedir=$HOME/Documenti/Note/
-Notelist=`ls $Notedir`
-#Notelist="$(echo $Notelist | sed -i 's/\s+/\n/g')"
+Notelist=`ls -t $Notedir`
 Notelist="$(echo $Notelist | tr " " "\n")"
 echo "$Notelist"
 Chosedfile="$(echo "$Notelist" | rofi -dmenu)"
 Nvimpath=$Notedir$Chosedfile
 echo $Nvimpath
-#nvim $Nvimpath
-#for f in $Notelist; do
-#    echo $f 
-#done 
-
-#echo $Notelist | rofi -dmenu
